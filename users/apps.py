56504c7d7bc1b3cd4d7b4103ee4f1f62d548d8b1
Anzutobi <Ferrous@88.com>
2025-12-12 20:25:10 +0800

    users模块中的models、apps和admin基本功能

--- users/apps.py
+++ users/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    verbose_name = '01. 人事与权限部'
--- users/models.py
+++ users/models.py
@@ -1,4 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Employee(AbstractUser):
+    POSITION_CHOICES = (
+        ('manager', '经理 - 全权管理'),
+        ('purchaser', '采购员 - 进货管理'),
+        ('warehouse', '库管员 - 库存盘点'),
+        ('sales', '销售员 - 销售出库'),
+        ('finance', '财务 - 统计报表'),
+    )
+    
+    real_name = models.CharField(max_length=20, verbose_name='真实姓名', default='')
+    mobile = models.CharField(max_length=11, verbose_name='手机号', blank=True, null=True)
+    position = models.CharField(max_length=20, choices=POSITION_CHOICES, default='sales', verbose_name='职位')
+
+    class Meta:
+        verbose_name = '员工'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.real_name if self.real_name else self.username
+
+    def save(self, *args, **kwargs):
+        # 只要是员工，默认都能登录后台（is_staff=True）
+        # 具体的权限（能不能看某个表）由 Django 的 Group 或后续业务逻辑控制
+        self.is_staff = True
+        super().save(*args, **kwargs)
