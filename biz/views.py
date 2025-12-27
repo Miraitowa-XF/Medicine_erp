6739d7a6a8a3d3bfca62bbe6858dba445fc825b1
yyy <3460833756@qq.com>
2025-12-27 14:33:47 +0800

    前端基本框架已搭建，manager部分功能，warehouse管理已完成

--- biz/views.py
+++ biz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import PurchaseOrder
 
-# Create your views here.
+@login_required
+def purchase_list(request):
+    """采购订单列表视图"""
+    # 获取所有采购单，预加载供应商和经办人
+    orders = PurchaseOrder.objects.select_related('supplier', 'employee').all().order_by('-order_date')
+    
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'biz/purchase_list.html', context)
